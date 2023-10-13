e3c2628ee10fb76dd68baef729ea0465dddceec3
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-10-13 08:24:16 +0700

    new feature

--- bot.js
+++ bot.js
@@ -1,16 +1,32 @@
+/*
+/*************************
+* Kalo mau pake tinggal pake aja 
+* jangan hapus sumber nya
+* Github : BOTCAHX 
+* Source Code : https://github.com/BOTCAHX/whatsapp-gpt
+**************************
+*/
+
 const qrcode = require('qrcode-terminal');
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const { platform } = require('node:os')
 const { askGPT, GptGo } = require('./lib/gpt_bot');
 const chalk = require('chalk');
+const { puppeteer } = require('puppeteer');
+const { fromBuffer } = require('file-type');
+const fetch = require('node-fetch');
+
+const BASE_URL = 'https://aemt.me'
 
 const client = new Client({
         authStrategy: new LocalAuth({
+        // proxyAuthentication: { username: 'username', password: 'password' },
         clientId: 'whatsapp-web',
         dataPath: './sessions'
     }),
     puppeteer: {
         headless: true,
+        // args: ['--proxy-server=proxy-server-that-requires-authentication.example.com'],
         args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
@@ -47,56 +63,100 @@ client.on('authenticated', () => {
 });
 
 client.on('auth_failure', message => {
+// jika pemulihan sesi tidak berhasil
     console.error('AUTHENTICATION FAILURE', message);
+});
+
+// auto reject jika telpon masuk
+client.on("call", async call => {
+  await call.reject();
+  await client.sendMessage(call.from, `\`\`\`This number can only receive text messages!\`\`\``)
 });
 
 client.on('ready', async () => {
     console.log(`${JSON.stringify(client.info)}`)
 });
+
 client.initialize();
 
+// message responses
 client.on("message", async (message) => {
   console.log(chalk.bgYellow.black(`${message.fromMe ? 'Me' : message.from}`));
   console.log(chalk.bgYellow.black(`> ${message.body}`));
 
   const text = message.body.toLowerCase();
-
+  // List fitur bot 
   if (text.includes(".ai")) {
-    try {
+    try {      
       const inputText = text.replace(".ai", "");
+      if (!inputText) await client.sendMessage(message.from, `Enter a Question!`)
+      message.react('⏳');
       const chats = await askGPT(inputText);      
       console.log(chalk.bgGreen.black(`> ${chats.result}`));     
       client.sendMessage(message.from, chats.result);
+      message.react('✔️');
     } catch (e) {
       console.log(e);
     }
-  } else if (text.includes(".gptgo")) {
+    } else if (text.includes(".gptgo")) {
     try {
       const inputText2 = text.replace(".gptgo", "");
+       if (!inputText2) await client.sendMessage(message.from, `Enter a Question!`)
+       message.react('⏳');
       const chats2 = await GptGo(inputText2);     
       console.log(chalk.bgGreen.black(`> ${chats2.result}`));
       client.sendMessage(message.from, chats2.result);
+      message.react('✔️');
     } catch (e) {
       console.log(e);
     }
-  } else if (text.includes(".sticker")) {
+    } else if (text.includes(".dalle")) {
+      try {
+        const inputText1 = text.replace(".dalle", "");
+         if (!inputText1) await client.sendMessage(message.from, `Enter parameter text!`)
+        message.react('⏳');
+        const res = await fetch(BASE_URL + `/dalle` + `?text=${encodeURIComponent(inputText1)}`).then(response => response.buffer())       
+        const response = new MessageMedia((await fromBuffer(res)).mime, res.toString("base64"))
+        await client.sendMessage(message.from, response, { caption: `Prompt : ${inputText1}` });
+        message.react('✔️');
+    } catch (e) {
+        console.log(e);
+    }
+     } else if (text.includes(".text2img")) {
+      try {        
+        const inputText2 = text.replace(".text2img", "");
+        if (!inputText2) await client.sendMessage(message.from, `Enter parameter text!`)
+        message.react('⏳');
+        const res = await fetch(BASE_URL + `/ai/text2img` + `?text=${encodeURIComponent(inputText2)}`).then(response => response.buffer());      
+        const response = new MessageMedia((await fromBuffer(res)).mime, res.toString("base64"))
+        await client.sendMessage(message.from, response, { caption: `Prompt : ${inputText2}` });
+        message.react('✔️');
+    } catch (e) {
+        console.log(e);
+    }    
+    } else if (text.includes(".sticker")) {
     try {
-      const quotedMsg = await message.getQuotedMessage();      
-      
+      const quotedMsg = await message.getQuotedMessage();
+      if (!quotedMsg) await client.sendMessage(message.from, `Send Images With Caption .sticker!`)                 
       if (quotedMsg && quotedMsg.hasMedia) {
+        message.react('⏳');
         const media = await quotedMsg.downloadMedia();
-        client.sendMessage(message.from, media, { sendMediaAsSticker: true, stickerAuthor: "BOTCAHX", stickerName: "Bot", stickerCategories: ["bot", "random"]});
+        client.sendMessage(message.from, media, { sendMediaAsSticker: true, stickerAuthor: "BOTCAHX", stickerName: "Bot", stickerCategories: ["🗿", "😆"]});
       }
     } catch (e) {
       console.log(e);
     }
-  } else if (text.includes(".menu")) {
+  } else if (text.includes(".menu")) {  // Help Prompt
     const r_menu = `
 ┌ *MENU*
 │ ◦ ai
 │ ◦ gptgo
+│ ◦ dalle
+│ ◦ text2img
+│ ◦ menu
 │ ◦ sticker
 └ `;
     client.sendMessage(message.from, r_menu);
   }
 });
+
