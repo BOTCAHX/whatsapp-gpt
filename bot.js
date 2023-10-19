d38023f3215ac0befc9522446ce15813e10d1619
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-10-20 03:25:10 +0700

    new base

--- bot.js
+++ bot.js
@@ -9,13 +9,13 @@
 const qrcode = require('qrcode-terminal');
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const { platform } = require('node:os');
+const os = require('os');
 const uploader = require('./lib/upload_file');
 const { askGPT, GptGo } = require('./lib/gpt_bot');
 const chalk = require('chalk');
 const { puppeteer } = require('puppeteer');
 const { fromBuffer } = require('file-type');
 const fetch = require('node-fetch');
-
 const BASE_URL = 'https://aemt.me'
 
 const emoji_loading = [
@@ -84,7 +84,7 @@ client.on('ready', async () => {
     console.log(`${JSON.stringify(client.info)}`)
 });
 
-client.initialize();
+client.initialize(); 
 
 // message responses
 client.on("message", async (message) => {
@@ -96,18 +96,48 @@ client.on("message", async (message) => {
   if (text.includes(".ai")) {
     try {      
       const inputText = text.replace(".ai", "");
-      if (!inputText) await client.sendMessage(message.from, `Enter a Question!`)
+      if (!inputText) throw message.reply('Enter questions!')
       message.react(react_loading);
       const chats = await askGPT(inputText);      
-      console.log(chalk.bgGreen.black(`> ${chats.result}`));     
-      client.sendMessage(message.from, chats.result);
+      console.log(chalk.bgGreen.black(`> ${chats.result}`)); 
+          
+      async function ctwa(title, description, thumbnail, mediaUrl) {
+        if (!title && !description && !thumbnail && !mediaUrl) {
+            const thumb = await MessageMedia.fromUrl('https://telegra.ph/file/3a34bfa58714bdef500d9.jpg');
+            return ({
+                "ctwaContext": {
+                    title: 'Simple WhatsApp Bot',
+                    description: `Library Version : ${require('whatsapp-web.js/package.json').version}`,
+                    thumbnail: thumb.data,
+                    mediaType: 2,
+                    mediaUrl: 'https://www.facebook.com/tio.permana.50999'
+                }
+            });
+        }
+        else return ({
+            ctwaContext: {
+                title: title,
+                description: description,
+                thumbnail: thumbnail,
+                mediaType: 2,
+                mediaUrl: mediaUrl
+            }
+        });
+    }
+
+    let fake = await ctwa();
+    fake.isForwarded = true;
+      await client.sendMessage(message.from, chats.result, {
+        quotedMessageId: message.id._serialized,
+        extra: fake
+    });
       message.react(react_done);
     } catch (e) {
       console.log(e);
     }
    } else if (text.includes(".c-ai")) {   
     const inputtext = text.replace(".c-ai", "");
-    if (!inputtext) await client.sendMessage(message.from, `Example: *.c-ai kirito|hai what is your name?*`);
+    if (!inputtext) throw message.reply('Example: *.c-ai kirito|hai what is your name?*')
     let [ prompt1, prompt2 ] = inputtext.split('|')
     if (!prompt1 || !prompt2) {
         await message.reply('Example: *.c-ai kirito|hai what is your name?*');
@@ -116,7 +146,37 @@ client.on("message", async (message) => {
         try {
             const chats = await fetch(BASE_URL + `/ai/c-ai` + `?prompt=${prompt1}` + `&text=${prompt2}`).then(res => res.json());
             console.log(chalk.bgGreen.black(`> ${chats.result}`));
-            await client.sendMessage(message.from, chats.result);
+            
+            async function ctwa(title, description, thumbnail, mediaUrl) {
+        if (!title && !description && !thumbnail && !mediaUrl) {
+            const thumb = await MessageMedia.fromUrl('https://telegra.ph/file/3a34bfa58714bdef500d9.jpg');
+            return ({
+                "ctwaContext": {
+                    title: 'Simple WhatsApp Bot',
+                    description: `Library Version : ${require('whatsapp-web.js/package.json').version}`,
+                    thumbnail: thumb.data,
+                    mediaType: 2,
+                    mediaUrl: 'https://www.facebook.com/tio.permana.50999'
+                }
+            });
+        }
+        else return ({
+            ctwaContext: {
+                title: title,
+                description: description,
+                thumbnail: thumbnail,
+                mediaType: 2,
+                mediaUrl: mediaUrl
+            }
+        });
+    }
+
+    let fake = await ctwa();
+    fake.isForwarded = true;
+      await client.sendMessage(message.from, chats.result, {
+        quotedMessageId: message.id._serialized,
+        extra: fake
+    });
             message.react(react_done);
         } catch (e) {
             console.log(e);
@@ -125,11 +185,41 @@ client.on("message", async (message) => {
     } else if (text.includes(".gptgo")) {
     try {
       const inputText = text.replace(".gptgo", "");
-       if (!inputText) await client.sendMessage(message.from, `Enter a Question!`)
+       if (!inputText) throw message.reply('Enter questions!')
        message.react(react_loading);
       const chats = await GptGo(inputText);     
       console.log(chalk.bgGreen.black(`> ${chats.result}`));
-      client.sendMessage(message.from, chats.result);
+      
+      async function ctwa(title, description, thumbnail, mediaUrl) {
+        if (!title && !description && !thumbnail && !mediaUrl) {
+            const thumb = await MessageMedia.fromUrl('https://telegra.ph/file/3a34bfa58714bdef500d9.jpg');
+            return ({
+                "ctwaContext": {
+                    title: 'Simple WhatsApp Bot',
+                    description: `Library Version : ${require('whatsapp-web.js/package.json').version}`,
+                    thumbnail: thumb.data,
+                    mediaType: 2,
+                    mediaUrl: 'https://www.facebook.com/tio.permana.50999'
+                }
+            });
+        }
+        else return ({
+            ctwaContext: {
+                title: title,
+                description: description,
+                thumbnail: thumbnail,
+                mediaType: 2,
+                mediaUrl: mediaUrl
+            }
+        });
+    }
+
+    let fake = await ctwa();
+    fake.isForwarded = true;
+      await client.sendMessage(message.from, chats.result, {
+        quotedMessageId: message.id._serialized,
+        extra: fake
+    });
       message.react(react_done);
     } catch (e) {
       console.log(e);
@@ -137,11 +227,11 @@ client.on("message", async (message) => {
     } else if (text.includes(".dalle")) {
       try {
         const inputText = text.replace(".dalle", "");
-         if (!inputText) await client.sendMessage(message.from, `Enter parameter text!`)
+         if (!inputText) throw message.reply('Input parameter text!')
         message.react(react_loading);
         const res = await fetch(BASE_URL + `/dalle` + `?text=${encodeURIComponent(inputText)}`).then(response => response.buffer())       
         const response = new MessageMedia((await fromBuffer(res)).mime, res.toString("base64"))
-        await client.sendMessage(message.from, response, { caption: `Prompt : ${inputText}` });
+        await client.sendMessage(message.from, response, { caption: `Prompt: ${inputText}`, quotedMessage: message.id._serialized });        
         message.react(react_done);
     } catch (e) {
         console.log(e);
@@ -162,7 +252,7 @@ client.on("message", async (message) => {
      } else if (text.includes(".removebg")) {
     try {
       const quotedMsg = await message.getQuotedMessage();
-      if (!quotedMsg) await client.sendMessage(message.from, `Reply image messages with caption *.removebg*`)                 
+      if (!quotedMsg) throw message.reply('Reply image messages with caption *.removebg*')          
       if (quotedMsg && quotedMsg.hasMedia) {
         message.react(react_loading);
         const media = await quotedMsg.downloadMedia();
@@ -172,7 +262,7 @@ client.on("message", async (message) => {
         console.log(chalk.bgGreen.black(`> ${remove.url.result}`));
         const imgs = await fetch(remove.url.result).then(res => res.buffer())
         const response = new MessageMedia((await fromBuffer(imgs)).mime, imgs.toString("base64"))
-        await client.sendMessage(message.from, response, { caption: '✔️ Done' });        
+        await client.sendMessage(message.from, response, { caption: `✔️ Done`, quotedMessage: message.id._serialized });               
         message.react(react_done);
       }
     } catch (e) {
@@ -181,7 +271,7 @@ client.on("message", async (message) => {
       } else if (text.includes(".remini")) {
     try {
       const quotedMsg = await message.getQuotedMessage();
-      if (!quotedMsg) await client.sendMessage(message.from, `Reply image messages with caption *.remini*`)                 
+          if (!quotedMsg) throw message.reply('Reply image messages with caption *.remini*')          
       if (quotedMsg && quotedMsg.hasMedia) {
         message.react(react_loading);
         const media = await quotedMsg.downloadMedia();
@@ -190,8 +280,8 @@ client.on("message", async (message) => {
         const remini = await fetch(BASE_URL + `/remini` + `?url=${cloud}` + `&resolusi=4`).then(response => response.json());
         console.log(chalk.bgGreen.black(`> ${remini.url.url}`));
         const imgs = await fetch(remini.url.url).then(res => res.buffer())
-        const response = new MessageMedia((await fromBuffer(imgs)).mime, imgs.toString("base64"))
-        await client.sendMessage(message.from, response, { caption: '✔️ Done' });        
+        const response = new MessageMedia((await fromBuffer(imgs)).mime, imgs.toString("base64"))        
+        await client.sendMessage(message.from, response, { caption: `✔️ Done`, quotedMessage: message.id._serialized });        
         message.react(react_done);
       }
     } catch (e) {
@@ -200,7 +290,7 @@ client.on("message", async (message) => {
       } else if (text.includes(".tozombie")) {
     try {
       const quotedMsg = await message.getQuotedMessage();
-      if (!quotedMsg) await client.sendMessage(message.from, `Reply image messages with caption  *.tozombie*`)                 
+      if (!quotedMsg) throw message.reply('Reply image messages with caption *.tozombie*')             
       if (quotedMsg && quotedMsg.hasMedia) {
         message.react(react_loading);
         const media = await quotedMsg.downloadMedia();
@@ -210,7 +300,7 @@ client.on("message", async (message) => {
         console.log(chalk.bgGreen.black(`> ${zombie.url}`));
         const imgs = await fetch(zombie.url).then(res => res.buffer())
         const response = new MessageMedia((await fromBuffer(imgs)).mime, imgs.toString("base64"))
-        await client.sendMessage(message.from, response, { caption: '✔️ Done' });        
+        await client.sendMessage(message.from, response, { caption: `✔️ Done`, quotedMessage: message.id._serialized });          
         message.react(react_done);
       }
     } catch (e) {
@@ -219,7 +309,7 @@ client.on("message", async (message) => {
       } else if (text.includes(".toanime")) {
     try {
       const quotedMsg = await message.getQuotedMessage();
-      if (!quotedMsg) await client.sendMessage(message.from, `Reply image messages with caption *.toanime*`)                 
+      if (!quotedMsg) throw message.reply('Reply image messages with caption *.toanime*')                     
       if (quotedMsg && quotedMsg.hasMedia) {
         message.react(react_loading);
         const media = await quotedMsg.downloadMedia();
@@ -229,7 +319,7 @@ client.on("message", async (message) => {
         console.log(chalk.bgGreen.black(`> ${anime.url.img_crop_single}`));
         const imgs = await fetch(anime.url.img_crop_single).then(res => res.buffer())
         const response = new MessageMedia((await fromBuffer(imgs)).mime, imgs.toString("base64"))
-        await client.sendMessage(message.from, response, { caption: '✔️ Done' });        
+        await client.sendMessage(message.from, response, { caption: `✔️ Done`, quotedMessage: message.id._serialized });               
         message.react(react_done);
       }
     } catch (e) {
@@ -238,7 +328,7 @@ client.on("message", async (message) => {
      } else if (text.includes(".tourl")) {
     try {
       const quotedMsg = await message.getQuotedMessage();
-      if (!quotedMsg) await client.sendMessage(message.from, `Reply image messages with caption *.tourl*`)                 
+      if (!quotedMsg) throw message.reply('Reply image/file messages with caption *.tourl*')          
       if (quotedMsg && quotedMsg.hasMedia) {
         message.react(react_loading);
         const media = await quotedMsg.downloadMedia();
@@ -257,11 +347,11 @@ Preview: ${cloud}`)
     } else if (text.includes(".text2img")) {
       try {        
         const inputText = text.replace(".text2img", "");
-        if (!inputText) await client.sendMessage(message.from, `Enter parameter text!`)
+        if (!inputText) throw message.reply('Enter parameter text!')
         message.react(react_loading);
         const res = await fetch(BASE_URL + `/ai/text2img` + `?text=${encodeURIComponent(inputText)}`).then(response => response.buffer());      
         const response = new MessageMedia((await fromBuffer(res)).mime, res.toString("base64"))
-        await client.sendMessage(message.from, response, { caption: `Prompt : ${inputText}` });
+        await client.sendMessage(message.from, response, { caption: `Prompt: ${inputText}`, quotedMessage: message.id._serialized });        
         message.react(react_done);
     } catch (e) {
         console.log(e);
@@ -269,17 +359,30 @@ Preview: ${cloud}`)
     } else if (text.includes(".info")) {
     try {
       let info = client.info;
+      let _uptime = process.uptime() * 1000
+      let timer = clockString(_uptime)
+      let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
         client.sendMessage(message.from, `
 ┌ *INFO*
+│ ◦ Runtime: ${timer}
 │ ◦ User name: ${info.pushname}
 │ ◦ My number: ${info.wid.user}
 │ ◦ Platform: ${info.platform}
 └
 `);
+    function clockString(ms) {
+    let days = Math.floor(ms / (24 * 60 * 60 * 1000));
+    let daysms = ms % (24 * 60 * 60 * 1000);
+    let hours = Math.floor((daysms) / (60 * 60 * 1000));
+    let hoursms = ms % (60 * 60 * 1000);
+    let minutes = Math.floor((hoursms) / (60 * 1000));
+    let minutesms = ms % (60 * 1000);
+    let sec = Math.floor((minutesms) / (1000));
+    return days + " d " + hours + "h " + minutes + "m " + sec + "s ";
+}
     } catch (e) {
       console.log(e);
-    }
-     } else if (text.includes(".groups")) {
+    } else if (text.includes(".groups")) {
     try {
         client.getChats().then(chats => {
             const groups = chats.filter(chat => chat.isGroup);
@@ -296,9 +399,11 @@ Preview: ${cloud}`)
         });
     } catch (e) {
         console.log(e);
-    }
-  } else if (text.includes(".menu")) {  // Help Prompt
+    }    
+  } else if (text.includes(".menu")) {  // Menu
     const r_menu = `
+◦ *Browser Version*  :  ${await client.getWWebVersion()}
+◦ *Library Version* : ${require('whatsapp-web.js/package.json').version}\n
 ┌ *MENU*
 │ ◦ ai
 │ ◦ gptgo
@@ -315,6 +420,37 @@ Preview: ${cloud}`)
 │ ◦ groups
 │ ◦ menu
 └ `;
-    client.sendMessage(message.from, r_menu);
-  }
+    async function ctwa(title, description, thumbnail, mediaUrl) {
+        if (!title && !description && !thumbnail && !mediaUrl) {
+            const thumb = await MessageMedia.fromUrl('https://telegra.ph/file/3a34bfa58714bdef500d9.jpg');
+            return ({
+                "ctwaContext": {
+                    title: 'Simple WhatsApp Bot',
+                    description: `Library Version : ${require('whatsapp-web.js/package.json').version}`,
+                    thumbnail: thumb.data,
+                    mediaType: 2,
+                    mediaUrl: 'https://www.facebook.com/tio.permana.50999'
+                }
+            });
+        }
+        else return ({
+            ctwaContext: {
+                title: title,
+                description: description,
+                thumbnail: thumbnail,
+                mediaType: 2,
+                mediaUrl: mediaUrl
+            }
+        });
+    }
+
+    let fake = await ctwa();
+    fake.isForwarded = true;
+    await client.sendMessage(message.from, r_menu, {
+        quotedMessageId: message.id._serialized,
+        extra: fake
+    });
+}
 });
+
+    
